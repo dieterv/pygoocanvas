72595daa4978fc160382aaa5cdb6ca333d982f79
Gustavo J. A. M. Carneiro <gjc@src.gnome.org>
2006-06-25 22:13:24 +0000

    Correct a couple of include paths.
    	* goocanvas.override: Correct a couple of include paths.
    
    	* Makefile.am: Update to use new pkg-config defined variable.
    
    	* configure.in: Merge all pkg-config checks in a single variable.
    
    	* arg-types.py: Register argtype for cairo_t* in reverse wrapper.
    
    
    
    svn path=/trunk/; revision=18

--- arg-types.py
+++ arg-types.py
@@ -1,4 +1,5 @@
 from argtypes import ArgType, matcher
+import reversewrapper
 
 class CairoMatrixArg(ArgType):
 
@@ -17,3 +18,18 @@ class CairoMatrixArg(ArgType):
         info.codeafter.append('   return PycairoMatrix_FromMatrix(ret);\n');
 
 matcher.register('cairo_matrix_t*', CairoMatrixArg())
+
+
+class CairoParam(reversewrapper.Parameter):
+    def get_c_type(self):
+        return self.props.get('c_type').replace('const-', 'const ')
+    def convert_c2py(self):
+        self.wrapper.add_declaration("PyObject *py_%s;" % self.name)
+        self.wrapper.write_code(
+            code=('py_%s = PycairoContext_FromContext(%s, NULL, NULL);' %
+                  (self.name, self.name)),
+            cleanup=("Py_DECREF(py_%s);" % self.name))
+        self.wrapper.add_pyargv_item("py_%s" % self.name)
+
+matcher.register_reverse("cairo_t*", CairoParam)
+
