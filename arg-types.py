387e47474a6d051f8cfbb7086ef82cae099186c8
Gustavo J. A. M. Carneiro <gjc@src.gnome.org>
2006-07-23 22:25:08 +0000

    cairorefcount fix
    svn path=/trunk/; revision=32

--- arg-types.py
+++ arg-types.py
@@ -26,7 +26,7 @@ class CairoParam(reversewrapper.Parameter):
     def convert_c2py(self):
         self.wrapper.add_declaration("PyObject *py_%s;" % self.name)
         self.wrapper.write_code(
-            code=('py_%s = PycairoContext_FromContext(%s, NULL, NULL);' %
+            code=('py_%s = PycairoContext_FromContext(cairo_reference(%s), NULL, NULL);' %
                   (self.name, self.name)),
             cleanup=("Py_DECREF(py_%s);" % self.name))
         self.wrapper.add_pyargv_item("py_%s" % self.name)
