106a2072e5d682f54a914b03b6c8cbab9d88322c
Gustavo J. A. M. Carneiro <gjc@src.gnome.org>
2007-01-01 19:00:27 +0000

    Add a new custom item example; this one is based on goocanvas.ItemSimple, and is actually a lot simpler!
    svn path=/trunk/; revision=129

---
+++ demo/custom-simple.py
@@ -0,0 +1,63 @@
+### A custom item example
+
+import sys
+import gobject
+import gtk
+import goocanvas
+
+
+
+class CustomRectItem(goocanvas.ItemSimple):
+
+    def __init__(self, x, y, width, height, **kwargs):
+        super(CustomRectItem, self).__init__(**kwargs)
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def do_create_path(self, cr):
+        cr.rectangle(self.x, self.y, self.width, self.height)
+        cr.move_to(self.x, self.y)
+        cr.line_to(self.x + self.width, self.y + self.height)
+        cr.move_to(self.x + self.width, self.y)
+        cr.line_to(self.x, self.y + self.height)
+
+gobject.type_register(CustomRectItem)
+
+
+def main(argv):
+    window = gtk.Window()
+    window.set_default_size(640, 600)
+    window.show()
+    window.connect("destroy", lambda w: gtk.main_quit())
+    
+    scrolled_win = gtk.ScrolledWindow()
+    scrolled_win.set_shadow_type(gtk.SHADOW_IN)
+    scrolled_win.show()
+    window.add(scrolled_win)
+    
+    canvas = goocanvas.Canvas()
+    canvas.set_size_request(600, 450)
+    canvas.set_bounds(0, 0, 1000, 1000)
+
+    root = canvas.get_root_item()
+
+    CustomRectItem(x=100, y=100, width=400, height=400,
+                   line_width=10, stroke_color="grey", parent=root)
+
+    goocanvas.Text(text="Hello World",
+                   x=300, y=300,
+                   anchor=gtk.ANCHOR_CENTER,
+                   font="Sans 24", parent=root).rotate(45, 300, 300)
+    
+    canvas.show()
+    scrolled_win.add(canvas)
+    
+
+    gtk.main()
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
